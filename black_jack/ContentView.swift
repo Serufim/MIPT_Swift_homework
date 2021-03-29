bdb5db174b99328250573053457a0a52fd7f3f49
Sergey Ufimtsev <s.ufimtsev@dodopizza.com>
2021-03-29 23:33:18 +0300

    Initial Commit

---
+++ black_jack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  black_jack
+//
+//  Created by Sergey Ufimtsev on 29.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ black_jack/black_jackApp.swift
@@ -0,0 +1,17 @@
+//
+//  black_jackApp.swift
+//  black_jack
+//
+//  Created by Sergey Ufimtsev on 29.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct black_jackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
